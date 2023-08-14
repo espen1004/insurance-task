095f198a811444aab81c18ed0559e00ca43743bf
Espen Nordli <espen.nordli@tietoevry.com>
2023-08-15 01:17:01 +0200

    add frontend

--- insurance-task-frontend/src/app/app.component.ts
+++ insurance-task-frontend/src/app/app.component.ts
@@ -1,10 +1,48 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormControl, FormGroup, Validators} from '@angular/forms';
+import {HttpClient} from "@angular/common/http";
+import {AgreementService} from "./agreement.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'insurance-task-frontend';
+
+  form: FormGroup;
+  submitted = false;
+
+  constructor(private http: HttpClient, private agreementService: AgreementService) {
+  }
+
+
+  ngOnInit() {
+    this.form = new FormGroup({
+      carRegistrationNumber: new FormControl('', [Validators.required]),
+      customerPid: new FormControl('', Validators.required),
+      firstName: new FormControl('', Validators.required),
+      lastName: new FormControl('', Validators.required),
+      email: new FormControl('', [Validators.required, Validators.email]),
+    });
+  }
+
+  next() {
+    this.submitted = true;
+    if (this.form.invalid) {
+      return;
+    }
+    this.agreementService.createAgreement(this.form.value).subscribe((response) => {
+      console.log(response);
+    });
+  }
+
+  get f(): { [key: string]: AbstractControl } {
+    return this.form.controls;
+  }
+
+  cancel() {
+    console.log("Not implemented")
+  }
 }
--- insurance-task-frontend/src/app/app.module.ts
+++ insurance-task-frontend/src/app/app.module.ts
@@ -2,15 +2,22 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
+import {AgreementService} from "./agreement.service";
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
-  providers: [],
+  providers: [
+    AgreementService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
